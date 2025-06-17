9ece09048fc22aa10075410aa6500a4896e7bdec
Satak Kumar Dey <130965098+satak100@users.noreply.github.com>
2025-06-17 15:29:42 +0600

    Add countFreq syscall and freqall command

---
+++ xv6-riscv/kernel/freq.h
@@ -0,0 +1,3 @@
+struct freq_array {
+  int counts[128];
+};
---
+++ xv6-riscv/user/freqall.c
@@ -0,0 +1,24 @@
+#include "kernel/types.h"
+#include "kernel/freq.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if(argc < 2){
+    printf("usage: freqall string\n");
+    exit(1);
+  }
+
+  struct freq_array arr;
+  if(countFreq(argv[1], &arr) < 0){
+    printf("countFreq syscall failed\n");
+    exit(1);
+  }
+  for(int i = 0; i < 128; i++){
+    if(arr.counts[i] > 0){
+      printf("%c: %d\n", i, arr.counts[i]);
+    }
+  }
+  exit(0);
+}
